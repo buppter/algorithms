510f6b30cf14125f3f8825999baf1e2d597a593a
smithxtian <shixintian@aliyun.com>
2019-08-12 13:25:02 +0800

    :bento: Adding some about LRU cache problems

---
+++ Leetcode/LRUCache/146_LRUCache.py
@@ -0,0 +1,55 @@
+"""
+author: buppter
+datetime: 2019/8/12 13:17
+
+
+题目描述：
+实现LRU cache
+lasted recently used cache
+
+Design and implement a data structure for Least Recently Used (LRU) cache.
+It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
+it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+"""
+from collections import OrderedDict
+
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.dic = OrderedDict()
+        self.remain = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key)
+        self.dic[key] = v
+        return v
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.dic:
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1
+            else:
+                self.dic.popitem(last=False)
+        self.dic[key] = value
