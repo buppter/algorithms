ad2f59939efe075afe595ca38b43438983993789
buppter <shixintian@aliyun.com>
2019-08-14 16:21:43 +0800

    :sparkles: Add Coin Change

--- Leetcode/DP/322_CoinChange.py
+++ Leetcode/DP/322_CoinChange.py
@@ -29,9 +29,8 @@ class Solution(object):
         res[0] = 0
         for i in range(1, amount + 1):
             for c in coins:
-                if i >=c:
+                if i >= c:
                     res[i] = min(res[i], res[i - c] + 1)
         if res[amount] == amount + 1:
             return -1
         return res[amount]
-
