7fba6fd08129018343e4f6be3d3f6b6da50a3ead
buppter <shixintian@bupt.edu.cn>
2019-08-15 20:42:30 +0800

    :sparkles: Add 左左转字符串

---
+++ TargetOffer/左旋转字符串.py
@@ -0,0 +1,20 @@
+"""
+author: buppter
+datetime: 2019-08-15 20:35
+
+
+题目描述
+汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。对于一个给定的字符序列S，
+请你把其循环左移K位后的序列输出。
+例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，搞定它
+"""
+
+
+class Solution:
+    def LeftRotateString(self, s: str, n: int) -> str:
+        if not s:
+            return s
+
+        n %= len(s)
+        return s[n:] + s[:n]
+
