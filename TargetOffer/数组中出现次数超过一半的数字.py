5e967b81f0f7058b11a6fa93b3173038af2f0775
buppter <shixintian@bupt.edu.cn>
2019-08-22 23:00:26 +0800

    :ok_hand: Update 数组中出现次数超过一半的数字

--- TargetOffer/数组中出现次数超过一半的数字.py
+++ TargetOffer/数组中出现次数超过一半的数字.py
@@ -8,12 +8,15 @@ datetime: 2019/8/14 16:01
 
 
 解决思路：
-排序，找到中位数，判断中位数的次数是否超过数组长度的一半
+1. 排序，找到中位数，判断中位数的次数是否超过数组长度的一半，时间复杂度O(nlogn)，不是最优
+2. 在遍历数组时保存两个值：一是数组中一个数字，一是次数。遍历下一个数字时，若它与之前保存的数字相同，则次数加1，否则次数减1；
+若次数为0，则保存下一个数字，并将次数置为1。遍历结束后，所保存的数字即为所求。然后再判断它是否符合条件即可。时间复杂度O(n)
+3. 统计数字的次数
 """
 
 
 class Solution:
-    def MoreThanHalfNum_Solution(self, numbers):
+    def MoreThanHalfNum_Solution1(self, numbers):
         if not numbers:
             return 0
         size = len(numbers)
@@ -23,4 +26,44 @@ class Solution:
         count = numbers.count(res)
         if count > size // 2:
             return res
+        return 0
+
+    def MoreThanHalfNum_Solution2(self, numbers):
+        if not numbers:
+            return 0
+
+        num = numbers[0]
+        count = 1
+        for i in numbers[1:]:
+            if i == num:
+                count += 1
+            else:
+                count -= 1
+
+            if count == 0:
+                num = i
+                count = 1
+
+        c = 0
+
+        for i in numbers:
+            if num == i:
+                c += 1
+        if c > len(numbers) // 2:
+            return num
+        return 0
+
+    def MoreThanHalfNum_Solution3(self, numbers):
+        if not numbers:
+            return 0
+        size = len(numbers)
+        res = {}
+        for i in numbers:
+            if i in res:
+                res[i] += 1
+            else:
+                res[i] = 1
+        for k, v in res.items():
+            if v > size // 2:
+                return k
         return 0
