3099b4d77fb922cf28d7e1edb64c672d59169640
buppter <shixintian@aliyun.com>
2019-08-25 11:45:52 +0800

    :sparkles: Add BFS

---
+++ BFS.py
@@ -0,0 +1,41 @@
+"""
+author: buppter
+datetime: 2019/8/25 10:09
+"""
+
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "C", "D"],
+    "C": ["A", "B", "D", "E"],
+    "D": ["B", "C"],
+    "E": ["C", "F"],
+    "F": ["E"]
+}
+
+
+def bfs(graph, start):
+    if not graph or not start:
+        return [], []
+
+    stack = [start]
+    seen = [start]
+    parent = {start: None}
+    res = []
+    while stack:
+        cur = stack.pop(0)
+
+        nodes = graph[cur]
+
+        for node in nodes:
+            if node not in seen:
+                stack.append(node)
+                seen.append(node)
+                parent[node] = cur
+        res.append(cur)
+    return res, parent
+
+
+if __name__ == "__main__":
+    res, parent = bfs(graph, "A")
+    print(res)
+    print(parent)
