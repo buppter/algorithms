5e78aa6566fb1d987ca4c98ba6a822e0c40e61c5
buppter <shixintian@aliyun.com>
2019-08-27 13:17:36 +0800

    :ok_hand: Update 最小的K个数

--- TargetOffer/最小的K个数.py
+++ TargetOffer/最小的K个数.py
@@ -8,7 +8,7 @@ datetime: 2019/8/14 16:14
 
 解决思路：
 1. 排序返回前4个数
-2. 建立小顶堆
+2. 建立大顶堆
 3. 快排思想
 """
 
@@ -20,7 +20,9 @@ class Solution:
         tinput.sort()
         return tinput[:k]
 
-    def GetLeastNumbers_Solution1(self, tinput, k):
+
+class Solution2:
+    def GetLeastNumbers_Solution(self, tinput, k):
         if not tinput or k > len(tinput) or not k:
             return []
         start = 0
@@ -51,3 +53,40 @@ class Solution:
             alist[high] = alist[low]
         alist[low] = mid
         return low
+
+
+class Solution3:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        if not tinput or not k or k > len(tinput):
+            return []
+
+        heap_k = tinput[:k]
+        self.build_heap(heap_k, k)
+
+        for i in tinput[k:]:
+            if i < heap_k[0]:
+                heap_k[0] = i
+                self.heapify(heap_k, k, 0)
+        return heap_k
+
+    def build_heap(self, alist, length):
+        last_node_index = length - 1
+        last_node_parent_index = (last_node_index - 1) // 2
+        for i in range(last_node_parent_index, -1, -1):
+            self.heapify(alist, length, i)
+
+    def heapify(self, alist, length, i):
+        if i > length:
+            return
+
+        max_index = i
+        l_node_index = 2 * i + 1
+        r_node_index = 2 * i + 2
+        if l_node_index < length and alist[l_node_index] > alist[max_index]:
+            max_index = l_node_index
+        if r_node_index < length and alist[r_node_index] > alist[max_index]:
+            max_index = r_node_index
+
+        if max_index != i:
+            alist[i], alist[max_index] = alist[max_index], alist[i]
+            self.heapify(alist, length, max_index)
