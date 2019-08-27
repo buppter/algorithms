8163d5ffa92940bb04c7f43d3080cb190c708da3
buppter <shixintian@aliyun.com>
2019-08-27 20:40:55 +0800

    :sparkles: Add 冒泡排序

--- Sort/bubble_sort.py
+++ Sort/bubble_sort.py
@@ -10,9 +10,8 @@ datetime: 2019/8/27 19:33
 def bubble_sort(alist):
     if not alist:
         return None
-    for i in range(len(alist) - 1):
-        for j in range(1 + i, 0, -1):
-            if alist[j] < alist[j - 1]:
-                alist[j], alist[j - 1] = alist[j - 1], alist[j]
-            else:
-                break
+
+    for i in range(len(alist)):
+        for j in range(len(alist) - 1 - i):
+            if alist[j] > alist[j + 1]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
