6abb666b77860a57ad7d950307824dbcc4374e60
buppter <shixintian@aliyun.com>
2019-09-16 10:17:31 +0800

    :sparkles: Add Min Stack

---
+++ LeetCodeHot100/155_Min_Stack.py
@@ -0,0 +1,29 @@
+"""
+author: buppter
+datetime: 2019/9/16 10:07
+"""
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        if self.stack:
+            cur = self.stack.pop()
+            if cur == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
