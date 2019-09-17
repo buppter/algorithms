5400e98d1b6b9ca57dd8d40babf46d35d872695d
buppter <shixintian@aliyun.com>
2019-09-17 15:58:57 +0800

    :ok_hand: Update another solution

--- TargetOffer/顺时针打印矩阵.py
+++ TargetOffer/顺时针打印矩阵.py
@@ -75,3 +75,20 @@ class Solution2:
         if new_matrix:
             self.printMatrix(new_matrix)
         return self.res
+
+
+class Solution3:
+
+    def printMatrix(self, matrix):
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            if matrix:
+                res += matrix.pop()[::-1]
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        return res
