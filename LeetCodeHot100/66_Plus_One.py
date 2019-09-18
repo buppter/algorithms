3b208a4b9c56f8e8e0eff71977c76ab916f374e0
buppter <shixintian@aliyun.com>
2019-09-18 17:08:28 +0800

    :sparkles: Add Plus One

---
+++ LeetCodeHot100/66_Plus_One.py
@@ -0,0 +1,24 @@
+"""
+author: buppter
+datetime: 2019/9/18 17:03
+"""
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+        carry = 0
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            if digits[i] == 10:
+                carry = digits[i] // 10
+                digits[i] %= 10
+            else:
+                return digits
+        if carry:
+            digits.insert(0, carry)
+        return digits
+
+
+if __name__ == '__main__':
+    print(Solution().plusOne([9,9]))
